3f88e23e7b9ea02680612c95530c20d14d9b35c9
spirobel <spirobel@protonmail.com>
2021-04-01 18:25:27 +0800

    frontend: show status and sync start stop buttons

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ require_dependency "discord_rolesync_constraint"
 
 DiscordRolesync::Engine.routes.draw do
   get "/sync" => "syncs#sync", constraints: DiscordRolesyncConstraint.new
+  get "/start" => "syncs#start", constraints: DiscordRolesyncConstraint.new
+  get "/stop" => "syncs#stop", constraints: DiscordRolesyncConstraint.new
   get "/botstats" => "syncs#botstats", constraints: DiscordRolesyncConstraint.new
 end
